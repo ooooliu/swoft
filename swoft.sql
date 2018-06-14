48aabea7eb5edbb4d373ec3c3fa5b7ab6b6be264
Lyn <yan252079467@vip.qq.com>
2018-06-14 15:59:37 +0800

    添加用户sql

---
+++ swoft.sql
@@ -0,0 +1,42 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50720
+ Source Host           : localhost
+ Source Database       : swoft
+
+ Target Server Type    : MySQL
+ Target Server Version : 50720
+ File Encoding         : utf-8
+
+ Date: 06/14/2018 15:54:36 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `password` varchar(128) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `user_name` varchar(256) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `INDEX_EMAIL` (`email`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='用户信息';
+
+-- ----------------------------
+--  Records of `user`
+-- ----------------------------
+BEGIN;
+INSERT INTO `user` (`email`, `password`, `user_name`) VALUES ('admin@qq.com', 'c3284d0f94606de1fd2af172aba15bf3', 'admin');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
